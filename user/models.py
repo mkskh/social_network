066c2620c25673540ee44300f55df5c5f9603dce
mkskh <maksym.kharchenko@dci-student.org>
2024-04-19 16:23:56 +0200

    created user profile

--- user/models.py
+++ user/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+
+    GENDER_CHOICES = [
+        ('MALE', 'Male'),
+        ('FEMALE', 'Female'),
+        ('OTHER', 'Other'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile')
+    date_of_modified = models.DateTimeField(auto_now=True)
+    date_of_birth = models.DateTimeField(blank=True)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    phone = models.CharField(max_length=15, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    image = models.ImageField(upload_to='user_images/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.user.username
