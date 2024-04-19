7f91d7ce9f8a5cf0d97f1ae9bb4057f79ba65a74
mkskh <maksym.kharchenko@dci-student.org>
2024-04-19 14:40:53 +0200

    user system

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-
+    path('login/', views.user_login, name='login'),
+    path('registration/', views.registration, name='registration'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import RegistrationForm, LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def registration(request):
+
+    if request.method == "GET":
+        form = RegistrationForm()
+        return render(request, 'user/registration.html', {"form": form})
+    
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('/user/login/')
+        else:
+            error = "You put wrong information. Please try again"
+            return render(request, '/user/registration.html', {"form": form, "error": error})
+
+
+def user_login(request):
+
+    if request.method == "GET":
+        return render(request, 'user/login.html', {})
+    
+    elif request.method == "POST":
+        
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, ("You have been logged in"))
+            return redirect('/')
+        else:
+            messages.error(request, ("Incorrect information were provided. Please try again"))
+            return render(request, 'user/login.html', {})
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, ("You have been logged out"))
+    return redirect('/')
