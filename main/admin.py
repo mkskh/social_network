49ca18f7913574f066f3d28ec1061a954b5f49b2
mkskh <maksym.kharchenko@dci-student.org>
2024-04-23 14:36:47 +0200

    created models: post, likes, comments

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Like, Comment, Post
 
-# Register your models here.
+
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Post)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True, blank=True) 
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    headline = models.CharField(max_length=100)
+    text = models.CharField(max_length=1000)
+    likes = models.ForeignKey(Like, on_delete=models.CASCADE)
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
