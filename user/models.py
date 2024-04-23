d9771501de789b0c75e0980f930288ebda6c6cb8
mkskh <maksym.kharchenko@dci-student.org>
2024-04-23 17:12:11 +0200

    added signal system for automatic creation UserProfile

--- user/models.py
+++ user/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
 
 
 class UserProfile(models.Model):
@@ -11,14 +10,14 @@ class UserProfile(models.Model):
         ('OTHER', 'Other'),
     ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile')
     date_of_modified = models.DateTimeField(auto_now=True, blank=True) 
-    date_of_birth = models.DateTimeField(blank=True)
-    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
-    phone = models.CharField(max_length=15, blank=True)
-    location = models.CharField(max_length=100, blank=True)
+    date_of_birth = models.DateTimeField(blank=True, null=True)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES, blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
     image = models.ImageField(upload_to='user_images/', blank=True, null=True)
-    description = models.CharField(max_length=300, blank=True)
+    description = models.CharField(max_length=300, blank=True, null=True)
     
     def __str__(self):
         return self.user.username
