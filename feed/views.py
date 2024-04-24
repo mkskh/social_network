a9335e76758eb329df5403a2d6972bcb5c8db0d9
mkskh <maksym.kharchenko@dci-student.org>
2024-04-24 23:45:18 +0200

    demo version of the news feed

--- feed/views.py
+++ feed/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from . import models
+from user.models import UserProfile
 
 
 def news_feed(request):
 
     posts = models.Post.objects.all()
+    profile = UserProfile.objects.get(user=request.user)
 
-    return render(request, 'feed/news_feed.html', {'posts': posts})
+    return render(request, 'feed/news_feed.html', {'posts': posts, 'profile': profile})
