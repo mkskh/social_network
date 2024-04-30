f7758ed82b58b098baf064746af69a979af13553
rituparnadobey-dci <rituparna.dobey@dci-student.org>
2024-04-30 16:09:14 +0200

    markrtplace

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'marketplace'
 
 urlpatterns = [
-
+    path('', views.marketplace_page, name='marketplace_page'),
+    path('about/', views.about, name='about'),
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def marketplace_page(request):
+    products = Product.objects.all()
+    return render(request, 'marketplace/marketplace.html', {'products':products})
+
+
+def about(request):
+    return render(request, 'marketplace/about.html')
