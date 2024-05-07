2dcf356f5c8e18aef95c7d103beb602e342f7b56
rituparnadobey-dci <rituparna.dobey@dci-student.org>
2024-05-07 14:48:07 +0200

    settings updated

--- social_network/settings.py
+++ social_network/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'social_network.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social_network',
+        'USER': 'social_network_admin',
+        'PASSWORD': 'test1234',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
