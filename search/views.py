b066c93fb1c6d8c3eabde25fe08a00dec135adf1
mkskh <maksym.kharchenko@dci-student.org>
2024-05-14 11:33:11 +0200

    In process of developing

--- search/views.py
+++ search/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+import random
+
+from user.models import UserProfile
+from .forms import SearchForm
 
 
 def search(request):
     
-    return render(request, 'search/search.html')
+    profile = UserProfile.objects.get(user=request.user)
+    form = SearchForm()
+
+    #recommended profiles
+    all_profiles = UserProfile.objects.all()
+
+    sorted_profiles_list = []
+
+    for profile_item in all_profiles:
+        if profile_item == profile:
+            pass
+        else:
+            sorted_profiles_list.append(profile_item)
+    
+    recommended_profiles = random.sample(sorted_profiles_list, 4)
+
+    return render(request, 'search/search.html', {'recommended_profiles': recommended_profiles, 'form': form})
