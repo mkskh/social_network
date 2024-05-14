5fac4561c11141f2d7762bacad5584bf634f2a6b
mkskh <maksym.kharchenko@dci-student.org>
2024-05-14 22:30:59 +0200

    search page finished, search system implemented

--- user/models.py
+++ user/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
+
 
 class UserProfile(models.Model):
 
@@ -11,19 +13,28 @@ class UserProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile')
     date_of_modified = models.DateTimeField(auto_now=True, blank=True) 
-    date_of_birth = models.DateTimeField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
     gender = models.CharField(max_length=6, choices=GENDER_CHOICES, blank=True, null=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
     image = models.ImageField(upload_to='user_images/', blank=True, null=True, default='user_images/dummy-profile-picture.png')
     description = models.CharField(max_length=300, blank=True, null=True)
     background_image = models.ImageField(upload_to='background_images/', blank=True, null=True, default='background_images/dummy_background_img.jpg')
+    
     def __str__(self):
         return self.user.username
+    
+    def age(self):
+        if self.date_of_birth:
+            today = datetime.today().date()
+            dob = self.date_of_birth
+            age = today.year - dob.year
+            return age
+        else:
+            return None
 
 
 
-# to add with migration if works
 class Album(models.Model):
     profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='albums')
     title = models.CharField(max_length=255)
@@ -43,3 +54,17 @@ class Photo(models.Model):
 
     def __str__(self):
         return f"Photo {self.id} in {self.album.title} by {self.album.profile.user.username}"
+
+
+class Subscribers(models.Model):
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.profile.user.username
+
+
+class Subscribed(models.Model):
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.profile.user.username
