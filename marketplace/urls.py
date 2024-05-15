473bdd3577cf6dac3210a06b0e2edbe5dda9bf3a
rituparnadobey-dci <rituparna.dobey@dci-student.org>
2024-05-15 13:41:17 +0200

    updating marketplace

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
-
+from .views import add_product
 
 app_name = 'marketplace'
 
 urlpatterns = [
     path('', views.marketplace_page, name='marketplace_page'),
-    path('about/', views.about, name='about'),
-    path('product/<int:pk>', views.product, name='product'),
+    path('product/<int:pk>/', views.product, name='product'),
+    path('category/<int:category_id>/', views.category, name='category'),
+    path('add_product/', views.add_product, name='add_product'),
+    path('added_products/', views.added_products, name='added_products'),
+    path('delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,16 +1,65 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product, Category
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import ProductForm
+
 
 # Create your views here.
+
+def category(request, category_id):
+    try:
+        category = Category.objects.get(id=category_id)
+        products = Product.objects.filter(category=category)
+        categories = Category.objects.all()  
+        return render(request, 'marketplace/category.html', {'products': products, 'category': category, 'categories': categories})
+    except Category.DoesNotExist:
+        messages.success(request, "That category doesn't exist!")
+        return redirect('marketplace_page')
+
 
 def product(request, pk):
     product = Product.objects.get(id=pk)
     return render(request, 'marketplace/product.html', {'product':product})
 
+
 def marketplace_page(request):
+    categories = Category.objects.all()
     products = Product.objects.all()
-    return render(request, 'marketplace/marketplace.html', {'products':products})
+    return render(request, 'marketplace/marketplace.html', {'categories': categories, 'products': products})
+
+@login_required
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.instance.seller = request.user
+            form.save()
+            messages.success(request, "Product added successfully!")
+            return redirect('marketplace:marketplace_page')  
+        else:
+            messages.error(request, "Error adding product. Please check your input.")
+    else:
+        form = ProductForm()
+    return render(request, 'marketplace/add_product.html', {'form': form})
 
 
-def about(request):
-    return render(request, 'marketplace/about.html')
+
+@login_required
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    product.delete()
+    messages.success(request, "Product deleted successfully!")
+    return redirect('marketplace:added_products')
+
+
+
+@login_required
+def added_products(request):
+
+        added_products = Product.objects.filter(seller=request.user)
+
+        
+        return render(request, 'marketplace/added_products.html', {'added_products': added_products})
