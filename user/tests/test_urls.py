f3f86e1291a711a85a624b418706a1b343d5d0aa
Jonathan-Lemarie <jonathan.maheux-lemarie@dci-student.org>
2024-05-31 13:56:52 +0200

    test user/urls done, test models and views ongoing

--- user/tests/test_urls.py
+++ user/tests/test_urls.py
@@ -9,37 +9,37 @@ from user import views
 class TestUrls(SimpleTestCase):
 
     def test_create_album_url(self):
-        url = reverse('create_album', args=[1])
+        url = reverse('user:create_album', args=[1])
         self.assertEqual(resolve(url).func, views.create_album)
 
     def test_album_list_url(self):
-        url = reverse('album_list', args=[1])
+        url = reverse('user:album_list', args=[1])
         self.assertEqual(resolve(url).func, views.album_list)
 
     def test_edit_album_url(self):
-        url = reverse('edit_album', args=[1, 1])
+        url = reverse('user:edit_album', args=[1, 1])
         self.assertEqual(resolve(url).func, views.edit_album)
 
     def test_delete_album_url(self):
-        url = reverse('delete_album', args=[1, 1])
+        url = reverse('user:delete_album', args=[1, 1])
         self.assertEqual(resolve(url).func, views.delete_album)
 
     def test_create_photo_url(self):
-        url = reverse('create_photo', args=[1, 1])
+        url = reverse('user:create_photo', args=[1, 1])
         self.assertEqual(resolve(url).func, views.create_photo)
 
     def test_photo_list_url(self):
-        url = reverse('photo_list', args=[1, 1])
+        url = reverse('user:photo_list', args=[1, 1])
         self.assertEqual(resolve(url).func, views.photo_list)
 
     def test_delete_photo_url(self):
-        url = reverse('delete_photo', args=[1, 1, 1])
+        url = reverse('user:delete_photo', args=[1, 1, 1])
         self.assertEqual(resolve(url).func, views.delete_photo)
 
     def test_edit_photo_url(self):
-        url = reverse('edit_photo', args=[1, 1, 1])
+        url = reverse('user:edit_photo', args=[1, 1, 1])
         self.assertEqual(resolve(url).func, views.edit_photo)
 
     def test_photo_gallery_url(self):
-        url = reverse('photo_gallery', args=[1, 1, 1])
+        url = reverse('user:photo_gallery', args=[1, 1, 1])
         self.assertEqual(resolve(url).func, views.photo_gallery)
