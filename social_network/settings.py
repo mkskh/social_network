d2338a3b35773890e70994a5a53bc9e1693c6ade
mkskh <maksym.kharchenko@dci-student.org>
2024-06-02 23:56:50 +0200

    implemented CI/CD

--- social_network/settings.py
+++ social_network/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 # aws settings
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID'),
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY'),
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME'),
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACC_KEY_ID'),
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SEC_ACC_KEY'),
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STOR_BUCKET_NAME'),
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
